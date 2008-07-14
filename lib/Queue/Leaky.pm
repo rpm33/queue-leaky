31984e3ebe46639b99e4cf374939b690367033ec
taro <taro@mikaka004.endeworks.jp>
2008-07-14 09:42:16 +0000

    first commit

---
+++ lib/Queue/Leaky.pm
@@ -0,0 +1,37 @@
+package Queue::Leaky;
+
+use strict;
+use 5.8.1;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Queue::Leaky -
+
+=head1 SYNOPSIS
+
+  use Queue::Leaky;
+
+=head1 DESCRIPTION
+
+Queue::Leaky is
+
+=head1 AUTHOR
+
+Taro Funaki E<lt>t@33rpm.jpE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
