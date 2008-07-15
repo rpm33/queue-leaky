01f3d30bffc1b5ea0d29c33beb2bcb722de35fa1
taro <taro@mikaka004.endeworks.jp>
2008-07-15 04:30:18 +0000

    make a skeleton code

--- lib/Queue/Leaky.pm
+++ lib/Queue/Leaky.pm
@@ -1,11 +1,42 @@
 package Queue::Leaky;
 
 use Moose;
+use Queue::Leaky::Types;
+
 our $VERSION = '0.01';
+
+has 'queue' => (
+    is       => 'rw',
+    does     => 'Queue::Leaky::Driver',
+    required => 1,
+    coerce   => 1,
+    handles => {
+        map { ( "q_$_" => $_ ) }
+            qw(next fetch insert clear)
+    },
+);
+
+has 'state' => (
+    is => 'rw',
+    does => 'Queue::Leaky::State',
+    coerce => 1,
+    required => 1,
+    handles => {
+        map { ("state_$_" => $_) } qw(get set remove incr decr)
+    }
+);
 
 __PACKAGE__->meta->make_immutable;
 
 no Moose;
+
+sub next { }
+
+sub fetch { }
+
+sub insert { }
+
+sub clear { }
 
 1;
 
@@ -23,6 +54,16 @@ Queue::Leaky - Queues with leaky buckets
 
   use Queue::Leaky;
   my $queue = Queue::Leaky->new;
+
+  $queue->inesrt( ... );
+
+  while ( 1 ) {
+    if ($queue->next) {
+      my $message = $queue->fetch;
+    }
+  }
+
+  $queue->clear;
 
 =head1 DESCRIPTION
 
